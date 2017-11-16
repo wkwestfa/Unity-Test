fc6e4c1fa9cdbde4ebc1376287daea79b2122c74
wkwestfa <wkwestfa@svsu.edu>
2017-11-16 00:51:58 -0500

    Chapter 10
    Just finished dealing with Triggers and the scripts and object that deal with them.

---
+++ Game Testing/Assets/Scripts/Exercises/Hour 10 - Triggers/MovementScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class MovementScript : MonoBehaviour {
+
+    /* Summary:
+     * Move an object using the mouse
+     */
+
+	// Update is called once per frame
+	void Update () {
+        float mouseX = Input.GetAxis("Mouse X") / 10;
+        float mouseY = Input.GetAxis("Mouse Y") / 10;
+
+        transform.Translate(mouseX, mouseY, 0f);
+	}
+}
---
+++ Game Testing/Assets/Scripts/Exercises/Hour 10 - Triggers/TriggerScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class TriggerScript : MonoBehaviour
+{
+    /* Summary:
+     * This script allows us to trigger an event
+     * upon one object colliding into another.
+     */
+
+    // When something enters the object this script is attached to
+    // other = The object that is entering
+    private void OnTriggerEnter(Collider other)
+    {
+        print(other.gameObject.name + " has entered the cube");
+    }
+
+    // When something stays inside the object this script is attached to
+    // other = The object that is entering
+    private void OnTriggerStay(Collider other)
+    {
+        print(other.gameObject.name + " has stayed in the cube");
+    }
+
+    // When something leaves the object this script is attached to
+    // other = The object that is entering
+    private void OnTriggerExit(Collider other)
+    {
+        print(other.gameObject.name + " has exited the cube");
+    }
+
+
+}
